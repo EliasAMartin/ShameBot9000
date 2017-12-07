fcad411943bf6e88d72fdcbf933b9e1f46cea9f0
UltraKeelan <keelan.lang@gmail.com>
2017-12-06 19:53:52 -0600

    "Real" initial commit.

---
+++ src/AutoShame.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+
+/**
+ * @author Keelan
+ *
+ */
+public class AutoShame {
+
+	/**
+	 * @param args
+	 * @author klang@illinois.edu
+	 * @author eliasam2@illinois.edu
+	 * 
+	 */
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
